108ac56f14c9d05e40b38dfe04307f14af49cb9d
Sunny <loganayers2013@yahoo.com>
2019-09-12 13:10:33 -0500

    updated orm.js

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,13 @@
+--Drop Database if it exists--
+DROP DATABASE IF EXISTS burgers_db;
 --Create burgers_db--
 CREATE DATABASE burgers_db;
 --Switch to or use burgers_db--
 USE burgers_db;
 --Create burgers table with following fields: id, burger_name, devoured--
 CREATE TABLE burgers(
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     burger_name VARCHAR(100) NOT NULL,
-    devoured BOOLEAN
-)
+    devoured BOOLEAN NOT NULL DEFAULT false,
+    PRIMARY KEY (id)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,10 @@
 --Populate burgers table with 3 enteries--
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES("Bacon Cheeseburger", false)
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES("Smashburger", false)
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES("Hamburger", false)
