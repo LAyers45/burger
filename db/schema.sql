2d6406d05e25aaaec002a1c5b262035df1a398b0
Sunny <loganayers2013@yahoo.com>
2019-09-11 21:06:34 -0500

    updated schema file

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,10 @@
 --Create burgers_db--
-
+CREATE DATABASE burgers_db;
 --Switch to or use burgers_db--
-
+USE burgers_db;
 --Create burgers table with following fields: id, burger_name, devoured--
-
+CREATE TABLE burgers(
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(100) NOT NULL,
+    devoured BOOLEAN
+)
